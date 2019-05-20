dff425cddb2a2f94611cc64a43110a931f2cf6cb
akurniawan <akurniawan@traveloka.com>
2019-05-20 11:14:24 +0700

    update parameter for language model loss

--- criterion.py
+++ criterion.py
@@ -4,22 +4,22 @@ _MEDIUM_TOKENS = 65000
 _HIGH_TOKENS = 500000
 
 
-def lm_criterion(params):
-    if params.weight_tying:
-        in_features = 2 * params.input_size \
-            if params.bidirectional else params.hidden_size
-    else:
-        in_features = 2 * params.hidden_size \
-            if params.bidirectional else params.hidden_size
+def lm_criterion(in_features, vocab_size):
+    # if weight_tying:
+    #     in_features = 2 * input_size \
+    #         if bidirectional else hidden_size
+    # else:
+    #     in_features = 2 * hidden_size \
+    #         if bidirectional else hidden_size
 
     splits = []
-    if params.vocab_size > _MEDIUM_TOKENS:
+    if vocab_size > _MEDIUM_TOKENS:
         splits = [2800, 20000, 760000]
-    elif params.vocab_size > _HIGH_TOKENS:
+    elif vocab_size > _HIGH_TOKENS:
         splits = [4200, 35000, 180000]
-    splits += [params.vocab_size - 2]
+    splits += [vocab_size - 2]
 
     criterion = nn.AdaptiveLogSoftmaxWithLoss(
-        in_features=in_features, n_classes=params.vocab_size, cutoffs=splits)
+        in_features=in_features, n_classes=vocab_size, cutoffs=splits)
 
     return criterion
