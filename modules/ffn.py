bd7d82f15561a53d0d386e3f5264a65b1211dba2
akurniawan <akurniawan@traveloka.com>
2019-05-20 11:13:36 +0700

    add positionwiseffn

---
+++ modules/ffn.py
@@ -0,0 +1,39 @@
+import torch
+import math
+import torch.nn as nn
+
+
+class Gelu(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self._c = math.sqrt(math.pi / 2)
+
+    def forward(self, x):
+        divisor = torch.tanh(self._c * (x + 0.044715 * x**3))
+        return 0.5 * x * (1 + divisor)
+
+
+class PositionWiseFFN(nn.Module):
+    def __init__(self, feature_size, num_units=2048, dropout=0.1):
+        super(PositionWiseFFN, self).__init__()
+        self._dropout = dropout
+        self.ffn = self._build_ffn(feature_size, num_units)
+
+    def _build_ffn(self, feature_size, num_units):
+        model = nn.Sequential(
+            nn.Linear(feature_size, num_units), Gelu(),
+            nn.Dropout(self._dropout), nn.Linear(num_units, feature_size),
+            nn.Dropout(self._dropout))
+        return model
+
+    def forward(self, X):
+        ffn = self.ffn(X)
+        # residual network
+        ffn += X
+
+        return ffn
+
+    def init_weight(self):
+        for idx in range(len(self.ffn)):
+            if hasattr(self.ffn[idx], "weight"):
+                nn.init.uniform_(self.ffn[idx].weight, -0.1, 0.1)
